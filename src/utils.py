4915bc3f5e911edd116305d182627804f23c2895
Garv321 <149756849+Garv321@users.noreply.github.com>
2025-06-08 21:48:06 +0530

    Deployemnt Configuration for aws

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,9 @@ import dill
 
 from src.exception import CustomException
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
+
+
 def save_object(file_path,obj):
     try:
         dir_path=os.path.dirname(file_path)
@@ -19,26 +22,40 @@ def save_object(file_path,obj):
         raise CustomException(e,sys)
     
 
-def evaluate_models(X_train,y_train,X_test,y_test,models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
     try:
-        report={}
+        report = {}
 
-        for i in range(len(list(models))):
-            model=list(models.values())[i]
+        for i in range(len(models)):
+            model_name = list(models.keys())[i]
+            model = models[model_name]
+            para = param[model_name]
 
-            model.fit(X_train,y_train)
+            gs = GridSearchCV(model, para, cv=3)
+            gs.fit(X_train, y_train)
 
-            y_train_pred=model.predict(X_train)
-            y_test_pred=model.predict(X_test)
+            model.set_params(**gs.best_params_)
+            model.fit(X_train, y_train)
 
-            train_model_score=r2_score(y_train,y_train_pred)
-            test_model_score=r2_score(y_test,y_test_pred)
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
 
-            report[list(models.keys())[i]]=test_model_score
-        
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[model_name] = test_model_score
 
         return report
-    
+
     except Exception as e:
-        raise CustomException(e,sys)
-        
+        raise CustomException(e, sys)
+
+
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
